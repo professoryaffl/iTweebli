1f41819de6513cf1d45af41ac3291c4f178e7952
Steve Baird <stevejbaird@gmail.com>
2014-09-05 16:58:36 +0100

    Initial Commit

---
+++ Tweebli/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tweebli
+//
+//  Created by Steve Baird on 05/09/2014.
+//  Copyright (c) 2014 Tweebli. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
